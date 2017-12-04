7f0c8ea4beeedaee12f7bfb584398cbd189df2c9
huyentrang <huyentranghathi@gmail.com>
2017-12-04 16:43:05 +0700

    add files in new branch

---
+++ anews/login.php
@@ -0,0 +1,16 @@
+<?php include 'inc/header.php'?>
+			<div class="content-left fl">
+				<?php include 'inc/left_bar.php'?>
+			</div>
+			<div class="content-right fr">
+				<h3>Đăng nhập</h3>
+				<div class="main-content">
+					<form action="" method="post">
+						Username: <input type="text" name="name" value="" /><br /><br />
+						Password: <input type="password" name="password" value="" /><br /><br />
+						
+						<input type="submit" name="submit" value="Đăng nhập" />
+					</form>
+				</div>
+			</div>
+<?php include 'inc/footer.php'?>
